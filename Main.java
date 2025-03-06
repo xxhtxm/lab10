14ed7176edb02b760f3e83a58f199523b872fce9
xxhtxm <denism2k7@gmail.com>
2025-03-06 10:13:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,116 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("мясо", "будка", "овчарка");
+        Cat cat = new Cat("корм", "дом", "белый");
+        Horse horse = new Horse("сено", "конюшня", "арабская");
+
+        Veterinar veter = new Veterinar();
+        veter.treatAnimal(dog);
+        veter.treatAnimal(cat);
+        veter.treatAnimal(horse);
+        dog.makeNoise();
+        dog.eat();
+        cat.makeNoise();
+        cat.eat();
+        horse.makeNoise();
+        horse.eat();
+    }
+}
+class Animal {
+    public String food;
+    public String location;
+
+    public Animal(String food, String location) {
+        this.food = food;
+        this.location = location;
+    }
+
+    public void makeNoise() {
+        System.out.println("Животное издает звук");
+    }
+
+    public void eat() {
+        System.out.println("Животное ест " + food);
+    }
+
+    public void sleep() {
+        System.out.println("Животное спит");
+    }
+}
+
+class Dog extends Animal {
+    private String poroda;
+
+    public Dog(String food, String location, String poroda) {
+        super(food, location);
+        this.poroda = poroda;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Собака лает.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Собака ест " + food);
+    }
+
+    public void fetch() {
+        System.out.println("Собака приносит мяч");
+    }
+}
+
+class Cat extends Animal {
+    private String color;
+
+    public Cat(String food, String location, String color) {
+        super(food, location);
+        this.color = color;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Кошка мяукает");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Кошка ест " + food);
+    }
+
+    public void scratch() {
+        System.out.println("Кошка точит когти");
+    }
+}
+
+class Horse extends Animal {
+    private String poroda;
+
+    public Horse(String food, String location, String poroda) {
+        super(food, location);
+        this.poroda = poroda;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Лошадь ржет");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Лошадь ест " + food);
+    }
+
+    public void gallop() {
+        System.out.println("Лошадь галопирует");
+    }
+}
+
+class Veterinar {
+    public void treatAnimal(Animal animal) {
+        System.out.println("Лечение животного:");
+        System.out.println("Еда животного: " + animal.food);
+        System.out.println("Местоположение животного: " + animal.location);
+    }
+}
